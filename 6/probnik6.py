bd1c45b12112eef3e793c97d8f5ff7a96a208ff4
Szhulanov <zstepan530@gmail.com>
2025-04-07 19:56:22 +0200

    6

---
+++ 6/probnik6.py
@@ -0,0 +1,37 @@
+from turtle import *
+
+screensize(5000, 5000)
+tracer(0)
+lt(90)
+
+m = 30
+
+for i in range(9):
+    fd(17 * m)
+    lt(90)
+    fd(8 * m)
+    lt(90)
+    fd(17 * m)
+
+up()
+lt(90)
+fd(3 * m)
+rt(90)
+fd(5 * m)
+lt(90)
+down()
+
+for i in range(4):
+    fd(97 * m)
+    rt(90)
+    fd(132 * m)
+    rt(90)
+
+up()
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        goto(x * m, y * m)
+        dot(8, 'white')
+
+update()
+done()
