a365422115654eb790f4417a01e6eb338cd8ec54
Szhulanov <zstepan530@gmail.com>
2025-06-02 12:04:20 +0200

    шпоры

---
+++ shpora/1/16367.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+matrix ='246 16 57 15 347 127 356'.split()
+graph ='EF FD CD CA AG GB BE FC BA'.split()
+print(*range(1, 8))
+for i in permutations('ABCDEFG'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+            print(*i)
