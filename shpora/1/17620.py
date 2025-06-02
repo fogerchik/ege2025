913b51dd2dbe3de19a6bdfd1e493d73090cdac57
Szhulanov <zstepan530@gmail.com>
2025-06-02 12:04:20 +0200

    шпоры

---
+++ shpora/1/17620.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+matrix = '256 134 267 27 16 135 34'.split()
+graph = 'AB BD DG GC CE EF FA FB ED'.split()
+print(*range(1,10))
+for i in permutations('ABCDEFG'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
