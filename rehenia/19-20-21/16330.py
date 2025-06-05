0645642717d0701468c969c07c0c5bdb11672a64
fogerchik <zstepan530@gmail.com>
2025-06-05 11:26:34 +0300

    Update 16330.py

--- rehenia/19-20-21/16330.py
+++ rehenia/19-20-21/16330.py
@@ -10,3 +10,22 @@ def f(x, y, s):
 print("19)",[s for s in range(1, 53) if f(6, s, 2)])
 print('20)',[s for s in range(1, 53) if f(6, s, 3)and not f(6,s,1)])
 print('21)',[s for s in range(1, 53) if f(6, s, 4)and not f(6,s,2)])
+
+
+# 1ОДНАКУЧА +7 *3  >=342 1<=S<=300 ВНАЧАЛЬЫНЙ МОМЕНТ
+def f(a, m):
+    if a >= 342:
+        return m % 2 == 0
+    if m == 0:
+        return 0
+    h = [f(a + 7, m - 1), f(a * 3, m - 1)]
+    if (m - 1) % 2 == 0:
+        return any(h)
+    else:
+        return all(h)
+
+
+for s in range(1, 301):
+    if f(s, 1) == 0 and f(s, 2) == 0 and f(s, 3) == 0 and f(s, 4) == 1 :
+        print(s)
+
